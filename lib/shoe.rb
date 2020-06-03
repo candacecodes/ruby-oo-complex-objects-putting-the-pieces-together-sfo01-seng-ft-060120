92c4b7304f490f9babbe2b8919c9ff73e4048a5c
eager-class-5814 <candacehchoe@gmail.com>
2020-06-03 20:18:33 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,13 @@
 # Make your shoe class here!
+
+class Shoe 
+   attr_accessor :brand, :color, :size, :material, :condition
+   
+   def initialize(shoe)
+    @shoe = shoe 
+  end 
+  
+  def cobble
+    puts "Your shoe is as good as new!"
+  end 
+end 
