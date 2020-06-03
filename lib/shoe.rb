85f3c3c2831f60db02fe515ce3f4b0d63ea360af
eager-class-5814 <candacehchoe@gmail.com>
2020-06-03 20:20:21 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -6,10 +6,10 @@ class Shoe
    def initialize(shoe)
     @shoe = shoe 
     @brand = "Nike"
-    @condition = "new" 
   end 
   
   def cobble
     puts "Your shoe is as good as new!"
+    @condition = "new"
   end 
 end 
